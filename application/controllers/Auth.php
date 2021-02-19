59f68e4704b9027581e001066c60ca2de6f01bb2
Nur Sasongko <32429721+ssasongko@users.noreply.github.com>
2021-02-19 11:09:04 +0700

    success movement

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -135,7 +135,7 @@ class Auth extends CI_Controller
         $this->session->unset_userdata('email');
         $this->session->unset_userdata('role_id');
 
-        $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Sucess Logout!</div>');
+        $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Success Logout!</div>');
         redirect('auth');
     }
 
